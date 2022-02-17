a1ae39432409070de5cd8af738108647c6a7c63c
abiyyu03 <abiyyu652@gmail.com>
2022-02-17 09:12:45 +0700

    add many alarm choices

--- timer.js
+++ timer.js
@@ -2,11 +2,22 @@ let intervals = 0;
 let checkTimerOrBreak = 0;
 //for check active function
 // let running = false;
+// validation 
+// if(document.getElementById("alarmBreak").value != "none" || 
+// document.getElementById("alarmTimer").value != "none" ||  
+// document.getElementById("setTimer").value != "none" || 
+// document.getElementById("setBreak").value != "none" ||  
+// document.getElementById("setIntervals").value != "none")
+// {
+//     document.getElementById("titleText").innerHTML = "Kamu sudah siap ?";
+//     document.getElementById("titleHeader").className = "card-header bg-success";
+// }
+
 function timer() 
 {
     let setTimer = parseInt(document.getElementById("setTimer").value);
     let audioBreak = document.getElementById("audioBreak");
-    audioBreak.src = "alarm/break.m4a"
+    audioBreak.src = "alarm/"+alarmBreak.value;
     //   running = !running;
     document.getElementById("title").innerHTML = "Focus - Pomotimer ";
     document.getElementById("titleText").innerHTML = "Yuk fokus bekerja..";
@@ -18,34 +29,23 @@ function timer()
     document.getElementById("setTimer").disabled = true;
     document.getElementById("setBreak").disabled = true;
     document.getElementById("setIntervals").disabled = true;
-    document.getElementById("labelTimer").innerHTML = setTimer + " : " + "00";
+    document.getElementById("labelTimer").innerHTML = setTimer + " : " + "0";
     
     checkTimerOrBreak+=1;
-
-    //validation 
-    // if(document.getElementById("alarmBreak").value == "none" || 
-    //     document.getElementById("alarmTimer").value == "none" ||  
-    //     document.getElementById("setTimer").value == "none" || 
-    //     document.getElementById("setBreak").value== "none" ||  
-    //     document.getElementById("setIntervals").value == "none")
-    // {
-    //     alert('Tidak boleh ada field yang kosong');
-    //     window.location.href = "index.html";
-    // }
-
+    
+    
     if(checkTimerOrBreak == 1){
         seconds = 60;
         setTimer -= 1;
     } else {
         setTimer -= 1;
         seconds = 59;
-            
+        
     }
-    // console.log(checkTimerOrBreak)
     
     runTimer = setInterval(function () {
         seconds--;
-        if (setTimer === 0 && seconds === -1) {
+        if (setTimer == 0 && seconds == -1) {
             clearTimeout(runTimer);
             audioBreak.play();
             breakTimer();
@@ -56,20 +56,21 @@ function timer()
         }
         document.getElementById("labelTimer").innerHTML =
         setTimer + " : " + seconds;
-    }, 200);
+    }, 1000);
 }
+
 function breakTimer() 
 {
     let setBreak = parseInt(document.getElementById("setBreak").value);
     let setIntervals = parseInt(document.getElementById("setIntervals").value);
     let audioTimer = document.getElementById("audioTimer");
-    audioTimer.src = "alarm/alarm.mp3";
-    //   running = !running;
-    document.getElementById("title").innerHTML = "Break - Pomotimer ";
+    audioTimer.src = "alarm/"+alarmTimer.value;
+    
+    document.getElementById("title").innerHTML = "Istirahat - Pomotimer ";
     document.getElementById("titleText").innerHTML = "Rileks dulu kawan..";
     document.getElementById("titleHeader").className = "card-header bg-secondary";
-    document.getElementById("labelTimer").innerHTML = setBreak + " : " + "00";
-
+    document.getElementById("labelTimer").innerHTML = setBreak + " : " + "0";
+    
     checkTimerOrBreak+=1;
     if(checkTimerOrBreak == 2)
     {
@@ -79,85 +80,107 @@ function breakTimer()
         setBreak -= 1;
         seconds = 59;
     }
-    // console.log(checkTimerOrBreak)
-
+    
     runBreak = setInterval(function () {
         seconds--;
-        if (setBreak === 0 && seconds === -1) {
+        if (setBreak == 0 && seconds == -1) {
             intervals += 1;
-            if (intervals === setIntervals) {
+            if (intervals == setIntervals) {
                 // alert(
                 //     "SELAMAT, KAMU TELAH MENJALANKAN TEKNIK POMODORO HINGGA TUNTAS. JANGAN LUPA DI EVALUASI YAA !"
                 //     );
-                    document.getElementById("buttonModal").click();
-                    clearTimeout(runBreak);
-                    return;
-                } else {
-                    audioTimer.play();
-                    clearTimeout(runBreak);
-                    timer();
-                }
+                document.getElementById("titleHeader").className = "card-header bg-success";
+                document.getElementById("titleText").innerHTML = "Atur Pomotimer";
+                document.getElementById("buttonStart").disabled = false;
+                document.getElementById("buttonReset").disabled = true;
+                document.getElementById("alarmBreak").disabled = false;
+                document.getElementById("alarmTimer").disabled = false;
+                document.getElementById("setTimer").disabled = false;
+                document.getElementById("setBreak").disabled = false;
+                document.getElementById("setIntervals").disabled = false;
+                document.getElementById("buttonModal").click();
+                clearTimeout(runBreak);
+                return;
+            } else {
+                audioTimer.play();
+                clearTimeout(runBreak);
+                timer();
             }
-            if (seconds == -1) {
-                setBreak -= 1;
-                seconds = 59;
-            }
-            document.getElementById("labelTimer").innerHTML =
-            setBreak + " : " + seconds;
-        }, 1000);
-    }
-    
-    // Reset Timer
-    function resetTimer() 
+        }
+        if (seconds == -1) {
+            setBreak -= 1;
+            seconds = 59;
+        }
+        document.getElementById("labelTimer").innerHTML =
+        setBreak + " : " + seconds;
+    }, 1000);
+}
+
+// Reset Timer
+function resetTimer() 
+{
+    if(checkTimerOrBreak == 1 || checkTimerOrBreak % 2 != 0)
     {
-        if(checkTimerOrBreak == 1 || checkTimerOrBreak % 2 != 0)
-        {
-            intervals = 0;
-            checkTimerOrBreak = 0;
-            clearInterval(runTimer);
-            document.getElementById("labelTimer").innerHTML = "00:00";
-            setTimer = "Set Minutes";
-            setBreak = "Set Minutes of Break";
-            document.getElementById("titleText").innerHTML = "Pomotimer Setup";
-            document.getElementById("titleHeader").className = "card-header bg-success";
-            document.getElementById("buttonReset").disabled = true;
-            document.getElementById("buttonStart").disabled = false;
-            document.getElementById("alarmBreak").disabled = false;
-            document.getElementById("alarmTimer").disabled = false;
-            document.getElementById("setTimer").disabled = false;
-            document.getElementById("setBreak").disabled = false;
-            document.getElementById("setIntervals").disabled = false;
-        } else if(checkTimerOrBreak % 2 == 0) {
-            intervals = 0
-            clearInterval(runBreak)
-            checkTimerOrBreak = 0;
-            setTimer = "Set Minutes"
-            setBreak = "Set Minutes of Break"
-            document.getElementById("titleText").innerHTML = "Pomotimer Setup";
-            document.getElementById("titleHeader").className = "card-header bg-success";
-            document.getElementById("labelTimer").innerHTML = "00:00";
-            document.getElementById("buttonReset").disabled = true;
-            document.getElementById("buttonStart").disabled = false;
-            document.getElementById("alarmBreak").disabled = false;
-            document.getElementById("alarmTimer").disabled = false;
-            document.getElementById("setTimer").disabled = false;
-            document.getElementById("setBreak").disabled = false;
-            document.getElementById("setIntervals").disabled = false;
-        }
+        intervals = 0;
+        checkTimerOrBreak = 0;
+        document.getElementById("labelTimer").innerHTML = "0:0";
+        setTimer = "Set Minutes";
+        setBreak = "Set Minutes of Break";
+        document.getElementById("title").innerHTML = "Atur Pomotimer";
+        document.getElementById("titleHeader").className = "card-header bg-success";
+        document.getElementById("titleText").innerHTML = "Atur Pomotimer";
+        document.getElementById("buttonReset").disabled = true;
+        document.getElementById("buttonStart").disabled = false;
+        document.getElementById("alarmBreak").disabled = false;
+        document.getElementById("alarmTimer").disabled = false;
+        document.getElementById("setTimer").disabled = false;
+        document.getElementById("setBreak").disabled = false;
+        document.getElementById("setIntervals").disabled = false;
+        clearInterval(runTimer);
+    } else if(checkTimerOrBreak % 2 == 0) {
+        intervals = 0
+        checkTimerOrBreak = 0;
+        setTimer = "Set Minutes"
+        setBreak = "Set Minutes of Break"
+        document.getElementById("titleText").innerHTML = "Atur Pomotimer";
+        document.getElementById("title").innerHTML = "Atur Pomotimer";
+        document.getElementById("titleHeader").className = "card-header bg-success";
+        document.getElementById("labelTimer").innerHTML = "0:0";
+        document.getElementById("buttonReset").disabled = true;
+        document.getElementById("buttonStart").disabled = false;
+        document.getElementById("alarmBreak").disabled = false;
+        document.getElementById("alarmTimer").disabled = false;
+        document.getElementById("setTimer").disabled = false;
+        document.getElementById("setBreak").disabled = false;
+        document.getElementById("setIntervals").disabled = false;
+        clearInterval(runBreak);
     }
-    
-    function enableStartButton() {
-        if (setTimer != "none" ) {
-            document.getElementById("buttonStart").disabled = false;
-        }
+}
+
+function enableStartButton() 
+{
+    if (setTimer != "none" ) {
+        document.getElementById("buttonStart").disabled = false;
     }
-    // / setTimer = new Date(today.getMonth(), today.getDate(), today.getFullYear(), today.getHours(), parseInt(timer), today.getSeconds()).getMinutes();
-    /**
-     * nilai awal 1.00
-     * nilai detik 59
-     * kurangi hingga sampai 0
-     * dari 0, menit berkurang 1 dan detik dimulai dari 59
-     * detik 0 dan menit 0, jalankan break, menit = 1, detik = 59
-     * dari 0, menit berkurang 1 dan detik dimulai dari 59
-     * detik 0 dan menit 0, jalankan timer, menit = 1, detik = 59
-     */
+}
+
+// function demoTimer()
+// {
+//     let audioTimer = document.getElementById("audioTimer");
+//     audioTimer.src = "alarm/"+alarmTimer.value;
+//     audioTimer.play();
+
+// }
+
+// function demoBreak()
+// {
+//     if(alarmBreak.value == "none")
+//     {
+//         audioBreak.stop();
+//     } else {
+//         let audioBreak = document.getElementById("audioBreak");
+//         audioBreak.src = "alarm/"+alarmBreak.value;
+//         audioBreak.play();
+//     }
+// }
+// / setTimer = new Date(today.getMonth(), today.getDate(), today.getFullYear(), today.getHours(), parseInt(timer), today.getSeconds()).getMinutes();
