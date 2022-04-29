575014bceea72ef0fac8a2ac87e0290f95d83be6
abiyyu03 <abiyyu652@gmail.com>
2022-04-29 13:32:19 +0700

    anotherrrr serviceworker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,19 +1,25 @@
-var staticCacheName = "pwa";
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/bootstrap.min.css',
+  '/js/bootstrap.min.js'
+];
 
-self.addEventListener("install", function (e) {
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
-
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
