3d65a8d7015079bbcc908016afecfcd414e15358
abiyyu03 <abiyyu652@gmail.com>
2023-05-24 05:20:00 +0700

    update serviceWorker

--- scripts/serviceWorker.js
+++ scripts/serviceWorker.js
@@ -30,10 +30,19 @@ self.addEventListener('install', (e) => {
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
+self.addEventListener('fetch', (e) => {
 	e.respondWith(
-		caches.match(e.request).then(function (response) {
-			return response || fetch(e.request);
-		}),
+		(async () => {
+			const r = await caches.match(e.request);
+			console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+			if (r) {
+				return r;
+			}
+			const response = await fetch(e.request);
+			const cache = await caches.open(cacheName);
+			console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+			cache.put(e.request, response.clone());
+			return response;
+		})(),
 	);
 });
