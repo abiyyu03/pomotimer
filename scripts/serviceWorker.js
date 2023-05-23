b64cd163434f1679cc2e22215c3a667595a4fc30
abiyyu03 <abiyyu652@gmail.com>
2023-05-24 00:06:05 +0700

    add cached file in serviceWorker

--- scripts/serviceWorker.js
+++ scripts/serviceWorker.js
@@ -7,6 +7,14 @@ var filesToCache = [
 	'../assets/css/bootstrap.min.css',
 	'../assets/js/bootstrap.min.js',
 	'../assets/js/jquery-3.6.0.min.js',
+	'../assets/illustration/break-illustration.png',
+	'../assets/illustration/work-illustration.png',
+	'../assets/icons/icon.png',
+	'../assets/alarm/alaskarobotics_1000-hz-beeps.wav',
+	'../assets/alarm/dcloh_alarm-clock.wav',
+	'../assets/alarm/kizilsungur_military-alarm.wav',
+	'../assets/alarm/rudmer-rotteveel_chicken-alarm-call-full-with-occasional-bird-sound.wav',
+	'../assets/alarm/splicesound_smoke-detector-alarm-close-perspective.wav',
 ];
 
 /* Start the service worker and cache all of the app's content */
